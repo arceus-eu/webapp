b232f08b694c6854fd65bf607d3c1cda4312327c
Pavel Sanatov <psanatovs@gmail.com>
2019-04-08 23:35:34 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Map from './components/map';
+
+class App extends Component {
+  render() {
+    return (
+      <Map />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/map/map.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class Map extends Component {
+  render() {
+    // Place map inside return(...);
+    return (
+      <div className="container">
+        <p className="h2">__MAP HERE__</p>
+      </div>
+    );
+  }
+}
+
+export default Map;
