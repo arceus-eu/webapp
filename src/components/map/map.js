a1a59a44d3a6e0f0fe5b6147b6b52c55f3d8181d
terencevanzoelen <terence@jaav.nl>
2019-04-11 15:46:26 +0200

    A loading map

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -2,22 +2,59 @@
 
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Map, TileLayer, type Viewport } from 'react-leaflet';
+import { Map, TileLayer, type Viewport, CircleMarker, GeoJSON } from 'react-leaflet';
+import  MarkerClusterGroup from 'react-leaflet-markercluster';
+import L from 'leaflet';
+
 import './map.css';
 
 require('react-leaflet-markercluster/dist/styles.min.css');
 
 const DEFAULT_VIEWPORT = {
     center: [53.21, 6.56],
-    zoom: 13
+    zoom: 13,
+    maxZoom: 20,
+    preferCanvas: true
 };
 
 export default class ViewportExample extends Component<
     {},
-    { viewport: Viewport },
+    { viewport: Viewport,
+      markers: GeoJSON
+    },
     > {
     state = {
         viewport: DEFAULT_VIEWPORT,
+        markers: []
+    };
+
+    componentDidMount = () => {
+
+        var me = this;
+
+        (async () => {
+            const response = await fetch('./data/groningen_trees_wgs84.geojson');
+
+            const data = await response.json();
+            const markers = <GeoJSON
+                data = {data}
+                pointToLayer = {(geoObj, latLng) => {
+                    return L.circleMarker(latLng, {
+                        radius: 2,
+                        color : '#226d29'
+                    })
+                    }
+                }
+                onEachFeature ={(feature, layer) => {
+                    layer.on('click', (tree) => {
+                        console.log(feature);
+                    })
+                    }
+                }
+                />;
+            me.setState({ markers });
+
+        })();
     };
 
     onClickReset = () => {
@@ -31,13 +68,23 @@ export default class ViewportExample extends Component<
     render() {
         return (
             <Map
+                ref="treemap"
                 onClick={this.onClickReset}
                 onViewportChanged={this.onViewportChanged}
+                maxZoom={20}
                 viewport={this.state.viewport}>
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+
+                <MarkerClusterGroup
+                    disableClusteringAtZoom={16}
+                    maxClusterRadius={60}
+                    spiderfyOnMaxZoom={false}
+                >
+                    {this.state.markers}
+                </MarkerClusterGroup>
             </Map>
         )
     }
